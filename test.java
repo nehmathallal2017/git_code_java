7f18c011b366ffd423f73b1088eed66d6cd71bcb
soap_web_services_NAH_AR <nehmathallal2017@hotmail.com>
2017-03-05 20:35:12 +0200

    Add files via upload

---
+++ test.java
@@ -0,0 +1,11 @@
+package chienna.pkg;
+
+public class test {
+
+	public static void main(String[] args) {
+		 mycalculator ca = new mycalculator();
+		 
+System.out.println("addd:" + ca.addd(5,6));
+	}
+
+}
